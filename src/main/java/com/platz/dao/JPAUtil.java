bc1fa6ff341845f6f968aa7e2ebd3efe55ffbc64
Christian <chrisxchris2010@hotmail.com>
2016-07-29 16:35:15 -0300

    Configuração do Hibernate OGM, criação do JPAUtil.

---
+++ src/main/java/com/platz/dao/JPAUtil.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.platz.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Administrador
+ */
+public class JPAUtil {
+    private EntityManagerFactory factory;
+    private static JPAUtil instance;
+
+    public JPAUtil() {
+        this.factory = Persistence.createEntityManagerFactory("platz");
+    }
+    public static synchronized  JPAUtil getInstance(){
+        if(instance == null){
+            instance = new JPAUtil();
+        }
+        return instance;                
+    }
+    public EntityManager getEntityManager(){
+        return  factory.createEntityManager();
+    }
+    
+    
+            
+}
