ee3468e76c19a3f1c8539eff4f370829a2558902
Anderson <15153766@LAB10-09.suicobrasileira.sp>
2016-07-29 13:50:06 -0300

    Configurando os arquivos

--- src/main/java/com/platz/dao/CategoriaDao.java
+++ src/main/java/com/platz/dao/CategoriaDao.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.platz.dao;
 
 /**
  *
- * @author 15153766
+ * @author Anderson
  */
 public class CategoriaDao {
     
