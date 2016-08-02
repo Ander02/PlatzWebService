7fb4f82b97b551e895a2f654231793c7c6aebe8e
15153766 <15153766@LAB10-18.suicobrasileira.sp>
2016-08-02 13:48:43 -0300

    Ajustes na CategoriaDao

--- src/main/java/com/platz/controller/CategoriaController.java
+++ src/main/java/com/platz/controller/CategoriaController.java
@@ -1,9 +1,39 @@
 package com.platz.controller;
 
+import com.platz.dao.CategoriaDao;
+import com.platz.model.Categoria;
+import java.util.List;
+
 /**
  *
  * @author Anderson
  */
 public class CategoriaController {
-    
+
+    private final CategoriaDao categoriaDao = new CategoriaDao();
+
+    public void cadastrar(Categoria entity) {
+        categoriaDao.cadastrar(entity);
+    }
+
+    public List<Categoria> listarTodos() {
+        return categoriaDao.listarTodos(Categoria.class);
+    }
+
+    public Categoria buscarPorId(Integer id) {
+        return categoriaDao.buscarPorId(Categoria.class, id);
+    }
+
+    public List<Categoria> buscarPeloNome(String nome) {
+        return categoriaDao.buscarPeloNome(nome);
+    }
+
+    public void alterar(Categoria entity) {
+        categoriaDao.alterar(entity);
+    }
+
+    public void excluir(Categoria entity) {
+        categoriaDao.excluir(entity);
+    }
+
 }
--- src/main/java/com/platz/dao/CategoriaDao.java
+++ src/main/java/com/platz/dao/CategoriaDao.java
@@ -1,9 +1,22 @@
 package com.platz.dao;
 
+import com.platz.model.Categoria;
+import java.util.List;
+import javax.persistence.EntityManager;
+
 /**
  *
  * @author Anderson
  */
-public class CategoriaDao {
-    
+public class CategoriaDao extends GenericDao<Categoria> {
+
+    @SuppressWarnings("unchecked")
+    public List<Categoria> buscarPeloNome(String nome) {
+        EntityManager entityManager = JPAUtil.getInstance().getEntityManager();
+        List<Categoria> lista = entityManager.createQuery("from Categoria where nome like :nome")
+                .setParameter("nome", nome + "%").getResultList();
+        entityManager.close();
+        return lista;
+    }
+
 }
