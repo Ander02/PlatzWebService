1967826829357e2ec90b41d22750b537fd98b898
Anderson <Jose da Cruz@Anderson-PC>
2016-08-16 21:42:49 -0300

    Buscar Mensagens pelo email

--- src/main/java/com/platz/service/MensagemService.java
+++ src/main/java/com/platz/service/MensagemService.java
@@ -21,9 +21,9 @@ import javax.ws.rs.core.Response;
  */
 @Path("")
 public class MensagemService {
-
+    
     private final MensagemController mensagemController = new MensagemController();
-
+    
     @POST
     @Path(value = "/mensagem")
     @Consumes(value = MediaType.APPLICATION_JSON)
@@ -31,18 +31,18 @@ public class MensagemService {
     public Response cadastrar(MensagemCadastro mensagem) {
         //Instanciar uma nova model
         MensagemModel model = new MensagemModel();
-
+        
         try {
             //Settar informações na model baseado na Conta de Cadastro passada
             model.setEmail(mensagem.getEmail());
             model.setConteudo(mensagem.getConteudo());
             model.setAssunto(new AssuntoController().buscarPorId(mensagem.getAssuntoId()));
-
+            
             mensagemController.cadastrar(model);
 
             // Retorna a resposta para o cliente com o Status Code CREATED e a Mensagem de Leitura
             return Response.status(Response.Status.CREATED).entity(new MensagemLeitura(model)).build();
-
+            
         } catch (Exception e) {
 
             //Envia erro pelo console
@@ -56,7 +56,7 @@ public class MensagemService {
     @Path(value = "/mensagens")
     @Produces(value = MediaType.APPLICATION_JSON + ";charset=UTF-8")
     public Response listarTodos() {
-
+        
         try {
             //Lista com todas as ContasaModels cadastradas
             List<MensagemModel> models = mensagemController.listarTodos();
@@ -64,13 +64,13 @@ public class MensagemService {
             List<MensagemLeitura> listaDeContas = new MensagemLeitura().converterLista(models);
             //Retorna a lista com um Status Code OK
             return Response.ok(listaDeContas).build();
-
+            
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
             //Retorna uma BadRequest ao usuário
             return Response.status(Response.Status.BAD_REQUEST).entity("Erro ao listar contas").build();
         }
-
+        
     }
     
     @GET
@@ -84,14 +84,32 @@ public class MensagemService {
 
             //Retorna um Status Code OK com a conta de leitura
             return Response.ok(new MensagemLeitura(model)).build();
-
+            
         }
 
         //Se a model for nula retorna um Status Code Not Found
         return Response.status(Response.Status.NOT_FOUND).entity("Conta não encontrada").build();
     }
     
-
-
-
+    @GET
+    @Path(value = "/mensagens/{email}")
+    @Produces(value = MediaType.APPLICATION_JSON + ";charset=UTF-8")
+    public Response buscarPeloEmail(@PathParam("email") String email) {
+        
+        try {
+            //Lista com todas as ContasaModels cadastradas
+            List<MensagemModel> models = mensagemController.buscarPeloEmail(email);
+            //Converter a lista de models para uma lista de leitura
+            List<MensagemLeitura> listaDeContas = new MensagemLeitura().converterLista(models);
+            //Retorna a lista com um Status Code OK
+            return Response.ok(listaDeContas).build();
+            
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+            //Retorna uma BadRequest ao usuário
+            return Response.status(Response.Status.BAD_REQUEST).entity("Erro ao listar contas").build();
+        }
+        
+    }
+    
 }
