ee850f4a154643672caa902b5898bf16d9ecfebc
15153770 <15153770@LAB10-02.suicobrasileira.sp>
2016-08-23 15:44:46 -0300

    Criação dos https de Postagem

---
+++ src/main/java/com/platz/http/edicao/PostagemEdicao.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.platz.http.edicao;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author 15153770
+ */
+@XmlRootElement
+public class PostagemEdicao {
+    private String conteudo;
+
+    public PostagemEdicao() {
+    }
+
+    public PostagemEdicao(String conteudo) {
+        setConteudo(conteudo);
+    }
+
+    
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+    
+    
+}
