239d00a080786da9b551f7a0027f5cb052b04c40
15153770 <15153770@LAB10-02.suicobrasileira.sp>
2016-08-23 15:55:19 -0300

    Criação dos https de Presenca

---
+++ src/main/java/com/platz/http/cadastro/PresencaCadastro.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.platz.http.cadastro;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author 15153770
+ */
+@XmlRootElement
+public class PresencaCadastro {
+    private String eventoId;
+    private String contaId;
+    private int tipoPresenca;
+
+    public PresencaCadastro() {
+    }
+
+    public PresencaCadastro(String eventoId, String contaId, int tipoPresenca) {
+        setEventoId(eventoId);
+        setContaId(contaId);
+        setTipoPresenca(tipoPresenca);
+    }
+    
+    public String getEventoId() {
+        return eventoId;
+    }
+
+    public void setEventoId(String eventoId) {
+        this.eventoId = eventoId;
+    }
+
+    public String getContaId() {
+        return contaId;
+    }
+
+    public void setContaId(String contaId) {
+        this.contaId = contaId;
+    }
+
+    public int getTipoPresenca() {
+        return tipoPresenca;
+    }
+
+    public void setTipoPresenca(int tipoPresenca) {
+        this.tipoPresenca = tipoPresenca;
+    }
+    
+    
+}
---
+++ src/main/java/com/platz/http/leitura/PresencaLeitura.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.platz.http.leitura;
+
+import com.platz.model.PresencaModel;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author 15153770
+ */
+@XmlRootElement
+public class PresencaLeitura {
+
+    private String id;
+    private EventoLeitura evento;
+    private ContaLeitura conta;
+    private String tipoPresenca;
+    private String dataCadastro;
+
+    public PresencaLeitura() {
+    }
+
+    public PresencaLeitura(PresencaModel model) {
+        setId(model.getId());
+        //setEvento(new EventoLeitura(model.getEvento()));
+        setConta(new ContaLeitura(model.getConta()));
+        setTipoPresenca(model.getTipoPresenca().getLabel());
+        setDataCadastro(model.getDataCadatro());
+    }
+
+    public PresencaLeitura(String id, EventoLeitura evento, ContaLeitura conta, String tipoPresenca, String dataCadastro) {
+        setId(id);
+        setEvento(evento);
+        setConta(conta);
+        setTipoPresenca(tipoPresenca);
+        setDataCadastro(dataCadastro);
+    }
+     //Métodos
+    public List<PresencaLeitura> converterLista(List<PresencaModel> modelList) {
+
+        List<PresencaLeitura> lista = new ArrayList<>();
+
+        for (PresencaModel model : modelList) {
+
+            PresencaLeitura postagem = new PresencaLeitura(model);
+            lista.add(postagem);
+        }
+        return lista;
+    }
+    
+    
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public EventoLeitura getEvento() {
+        return evento;
+    }
+
+    public void setEvento(EventoLeitura evento) {
+        this.evento = evento;
+    }
+
+    public ContaLeitura getConta() {
+        return conta;
+    }
+
+    public void setConta(ContaLeitura conta) {
+        this.conta = conta;
+    }
+
+    public String getTipoPresenca() {
+        return tipoPresenca;
+    }
+
+    public void setTipoPresenca(String tipoPresenca) {
+        this.tipoPresenca = tipoPresenca;
+    }
+
+    public String getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(String dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+}
