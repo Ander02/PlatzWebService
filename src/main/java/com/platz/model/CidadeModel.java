f257a9725845e6b79e9d9045d5ec9a6a5bab151a
Anderson <15153766@LAB10-09.suicobrasileira.sp>
2016-08-24 16:57:06 -0300

    Criação do Service de Cidade

--- src/main/java/com/platz/model/CidadeModel.java
+++ src/main/java/com/platz/model/CidadeModel.java
@@ -5,6 +5,8 @@
  */
 package com.platz.model;
 
+import com.platz.dao.EstadoDao;
+import com.platz.http.cadastro.CidadeCadastro;
 import com.platz.util.DataUtil;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -22,6 +24,7 @@ import org.bson.types.ObjectId;
 @Entity
 @Table(name = "cidade")
 public class CidadeModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private ObjectId id;    
@@ -30,39 +33,44 @@ public class CidadeModel {
     @NotNull(message = "O estado deve ser informado")
     @ManyToOne
     private EstadoModel estado;
-
+    
     public CidadeModel() {
     }
+    
+    public CidadeModel(CidadeCadastro cidade) {
+        setNome(cidade.getNome());
+        setEstado(new EstadoDao().buscarPorId(EstadoModel.class, cidade.getEstadoId()));
+    }
 
-    
-    
     //getters and setters
     public String getId() {
         return id.toHexString();
     }
-
+    
     public void setId(String id) {
         this.id = new ObjectId(id);
     }
-
+    
     public ObjectId getObjectId() {
         return this.id;
     }
+
     public String getNome() {
         return nome;
     }
-
+    
     public void setNome(String nome) {
         this.nome = nome;
     }
-
+    
     public EstadoModel getEstado() {
         return estado;
     }
-
+    
     public void setEstado(EstadoModel estado) {
         this.estado = estado;
     }
+
     public String getDataCadastro() {
         return new DataUtil().converterData(id.getDate());
     }
--- src/main/java/com/platz/service/CidadeService.java
+++ src/main/java/com/platz/service/CidadeService.java
@@ -1,6 +1,17 @@
 package com.platz.service;
 
+import com.platz.controller.CidadeController;
+import com.platz.http.cadastro.CidadeCadastro;
+import com.platz.http.leitura.CidadeLeitura;
+import com.platz.model.CidadeModel;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -8,7 +19,52 @@ import javax.ws.rs.Path;
  */
 @Path("")
 public class CidadeService {
-    
-    
-    
+
+    private final CidadeController cidadeController = new CidadeController();
+
+    @POST
+    @Path(value = "/assunto")
+    @Consumes(value = MediaType.APPLICATION_JSON)
+    @Produces(value = MediaType.APPLICATION_JSON + ";charset=UTF-8")
+    public Response cadastrar(CidadeCadastro cidade) {
+
+        try {
+            // Settar o nome da model baseado no nome do assunto passado
+            CidadeModel model = new CidadeModel();
+
+            // Cadastrar assunto
+            cidadeController.cadastrar(model);
+
+            // Retorna a resposta para o cliente com o Status Code CREATED e o Assunto de Leitura
+            return Response.status(Response.Status.CREATED).entity(new CidadeLeitura(model)).build();
+
+        } catch (Exception e) {
+            // Envia erro pelo console
+            System.out.println("Erro: " + e.getMessage());
+            //Retorna uma BadRequest ao usuário
+            return Response.status(Response.Status.BAD_REQUEST).entity("Erro ao cadastrar assunto").build();
+        }
+    }
+
+    @GET
+    @Path(value = "/cidades")
+    @Produces(value = MediaType.APPLICATION_JSON + ";charset=UTF-8")
+    public Response listarTodos() {
+        try {
+            //Lista com todas as AssuntoEntity cadastradas
+            List<CidadeModel> models = cidadeController.listarTodos();
+
+            //Lista de Assuntos de Leitura baseado na lista de models
+            List<CidadeLeitura> listaDeAssuntos = new CidadeLeitura().converterLista(models);
+
+            //Retorna a lista com um Status Code OK
+            return Response.ok(listaDeAssuntos).build();
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+            //Retorna uma BadRequest ao usuário
+            return Response.status(Response.Status.BAD_REQUEST).entity("Erro ao listar cidades").build();
+        }
+    }
+
 }
