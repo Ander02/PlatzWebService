8166ef5de6157aaf94a6032494d1e278b76f2c0e
Anderson <15153766@LAB10-09.suicobrasileira.sp>
2016-08-25 16:14:24 -0300

    Conclusão de Endereço para Usuário

--- src/main/java/com/platz/http/leitura/EnderecoLeitura.java
+++ src/main/java/com/platz/http/leitura/EnderecoLeitura.java
@@ -5,6 +5,7 @@
  */
 package com.platz.http.leitura;
 
+import com.platz.model.EnderecoModel;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -13,5 +14,73 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement
 public class EnderecoLeitura {
+    
+    private String cep;
+    private String rua;
+    private String bairro;
+    private String numero;
+    private String complemento;
+    private CidadeLeitura cidade;
+    
+    public EnderecoLeitura() {
+    }
+    
+    public EnderecoLeitura(EnderecoModel model) {
+        setCep(model.getCep());
+        setRua(model.getRua());
+        setBairro(model.getBairro());        
+        setNumero(model.getNumero());
+        setComplemento(model.getComplemento());
+        setCidade(new CidadeLeitura(model.getCidade()));
+    }
+
+    //Getters and Setters
+    public String getCep() {
+        return cep;
+    }
+    
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    
+    public String getRua() {
+        return rua;
+    }
+    
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+    
+    public String getBairro() {
+        return bairro;
+    }
+    
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    
+    public String getNumero() {
+        return numero;
+    }
+    
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
+    public String getComplemento() {
+        return complemento;
+    }
+    
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    
+    public CidadeLeitura getCidade() {
+        return cidade;
+    }
+    
+    public void setCidade(CidadeLeitura cidade) {
+        this.cidade = cidade;
+    }
     
 }
--- src/main/java/com/platz/http/leitura/UsuarioLeitura.java
+++ src/main/java/com/platz/http/leitura/UsuarioLeitura.java
@@ -3,13 +3,15 @@ package com.platz.http.leitura;
 import com.platz.model.UsuarioModel;
 import java.util.ArrayList;
 import java.util.List;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
  * @author 15153770
  */
+@XmlRootElement
 public class UsuarioLeitura {
-
+    
     private String id;
     private String nome;
     private String cpf;
@@ -18,10 +20,11 @@ public class UsuarioLeitura {
     private String dataNascimento;
     private String dataCadastro;
     private ContaLeitura conta;
-
+    private EnderecoLeitura endereco;
+    
     public UsuarioLeitura() {
     }
-
+    
     public UsuarioLeitura(UsuarioModel model) {
         setId(model.getId());
         setNome(model.getNome());
@@ -31,94 +34,92 @@ public class UsuarioLeitura {
         setDataNascimento(model.getDataNascimento());
         setDataCadastro(model.getDataCadastro());
         setConta(new ContaLeitura(model.getConta()));
-    }
-
-    public UsuarioLeitura(String id, String nome, String cpf, String imagemPerfil, String telefone, String dataNascimento, String dataCadastro, ContaLeitura conta) {
-        setId(id);
-        setNome(nome);
-        setCpf(cpf);
-        setImagemPerfil(imagemPerfil);
-        setTelefone(telefone);
-        setDataNascimento(dataNascimento);
-        setDataNascimento(dataNascimento);
-        setConta(conta);
+        setEndereco(new EnderecoLeitura(model.getEndereco()));
     }
 
     //Métodos
     public List<UsuarioLeitura> converterLista(List<UsuarioModel> modelList) {
-
+        
         List<UsuarioLeitura> lista = new ArrayList<>();
-
+        
         for (UsuarioModel model : modelList) {
-
+            
             UsuarioLeitura usuario = new UsuarioLeitura(model);
             lista.add(usuario);
         }
         return lista;
     }
-
+    
     public String getId() {
         return id;
     }
-
+    
     public void setId(String id) {
         this.id = id;
     }
-
+    
     public String getNome() {
         return nome;
     }
-
+    
     public void setNome(String nome) {
         this.nome = nome;
     }
-
+    
     public String getCpf() {
         return cpf;
     }
-
+    
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-
+    
     public String getTelefone() {
         return telefone;
     }
-
+    
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
-
+    
     public String getDataNascimento() {
         return dataNascimento;
     }
-
+    
     public void setDataNascimento(String dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
-
+    
     public String getDataCadastro() {
         return dataCadastro;
     }
-
+    
     public void setDataCadastro(String dataCadastro) {
         this.dataCadastro = dataCadastro;
     }
-
+    
     public ContaLeitura getConta() {
         return conta;
     }
-
+    
     public void setConta(ContaLeitura conta) {
         this.conta = conta;
     }
-
+    
     public String getImagemPerfil() {
         return imagemPerfil;
     }
-
+    
     public void setImagemPerfil(String imagemPerfil) {
         this.imagemPerfil = imagemPerfil;
     }
-
+    
+    public EnderecoLeitura getEndereco() {
+        return endereco;
+    }
+    
+    public void setEndereco(EnderecoLeitura endereco) {
+        this.endereco = endereco;
+    }
+    
 }
