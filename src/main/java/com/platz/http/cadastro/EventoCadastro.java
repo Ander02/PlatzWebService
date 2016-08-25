93ad52ae25bc8d59d7ebe3c5240e8cfc647cae5e
15153770 <15153770@LAB10-02.suicobrasileira.sp>
2016-08-25 16:14:48 -0300

    Edição de Eventocadastro

--- src/main/java/com/platz/http/cadastro/EventoCadastro.java
+++ src/main/java/com/platz/http/cadastro/EventoCadastro.java
@@ -1,5 +1,6 @@
 package com.platz.http.cadastro;
 
+import java.util.List;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -8,7 +9,152 @@ import javax.xml.bind.annotation.XmlRootElement;
  */
 @XmlRootElement
 public class EventoCadastro {
-    
-        
-    
+
+    private String nome;
+    private String detalhes;
+    private int idade;
+    private String dataFim;
+    private String dataInicio;
+    private Integer lotacaoMin;
+    private Integer lotacaoMax;
+    private Double preco;
+    private String empresaId;
+    private List<String> categoriasId;
+    private List<String> imagensId;
+    private String cancelado;
+    private String censurado;
+    private Boolean destaque;
+
+    public EventoCadastro() {
+    }
+
+    public EventoCadastro(String nome, String detalhes, int idade, String dataFim, String dataInicio, Integer lotacaoMin, Integer lotacaoMax, Double preco, String empresaId, List<String> categoriasId, List<String> imagensId, String cancelado, String censurado, Boolean destaque) {
+        setNome(nome);
+        setDetalhes(detalhes);
+        setDestaque(destaque);
+        setIdade(idade);
+        setDataFim(dataFim);
+        setDataInicio(dataInicio);
+        setLotacaoMax(lotacaoMax);
+        setLotacaoMin(lotacaoMin);
+        setPreco(preco);
+        setEmpresaId(empresaId);
+        setCategoriasId(categoriasId);
+        setImagensId(imagensId);
+        setCancelado(cancelado);
+        setCensurado(censurado);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDetalhes() {
+        return detalhes;
+    }
+
+    public void setDetalhes(String detalhes) {
+        this.detalhes = detalhes;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(String dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public String getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(String dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Integer getLotacaoMin() {
+        return lotacaoMin;
+    }
+
+    public void setLotacaoMin(Integer lotacaoMin) {
+        this.lotacaoMin = lotacaoMin;
+    }
+
+    public Integer getLotacaoMax() {
+        return lotacaoMax;
+    }
+
+    public void setLotacaoMax(Integer lotacaoMax) {
+        this.lotacaoMax = lotacaoMax;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getEmpresaId() {
+        return empresaId;
+    }
+
+    public void setEmpresaId(String empresaId) {
+        this.empresaId = empresaId;
+    }
+
+    public List<String> getCategoriasId() {
+        return categoriasId;
+    }
+
+    public void setCategoriasId(List<String> categoriasId) {
+        this.categoriasId = categoriasId;
+    }
+
+    public List<String> getImagensId() {
+        return imagensId;
+    }
+
+    public void setImagensId(List<String> imagensId) {
+        this.imagensId = imagensId;
+    }
+
+    public String getCancelado() {
+        return cancelado;
+    }
+
+    public void setCancelado(String cancelado) {
+        this.cancelado = cancelado;
+    }
+
+    public String getCensurado() {
+        return censurado;
+    }
+
+    public void setCensurado(String censurado) {
+        this.censurado = censurado;
+    }
+
+    public Boolean getDestaque() {
+        return destaque;
+    }
+
+    public void setDestaque(Boolean destaque) {
+        this.destaque = destaque;
+    }
+
 }
