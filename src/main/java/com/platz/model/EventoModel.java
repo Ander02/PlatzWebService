19633d8e29d1d780f26ad9aa8704ac15df7b4ec8
Anderson <15153766@LAB10-09.suicobrasileira.sp>
2016-08-31 16:32:49 -0300

    buscar por categoria (não concluído)

--- src/main/java/com/platz/model/EventoModel.java
+++ src/main/java/com/platz/model/EventoModel.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
