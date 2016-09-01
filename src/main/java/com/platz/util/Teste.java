78983a9e1244f5321bb3b7388675e4361d8f5051
15153770 <15153770@LAB10-02.suicobrasileira.sp>
2016-09-01 16:40:11 -0300

    retirada de imports

--- src/main/java/com/platz/util/Teste.java
+++ src/main/java/com/platz/util/Teste.java
@@ -1,7 +1,8 @@
 package com.platz.util;
 
-import java.util.Calendar;
-import java.util.Date;
+import com.platz.dao.EventoDao;
+import com.platz.http.cadastro.CategoriaCadastro;
+import com.platz.model.CategoriaModel;
 
 /**
  *
@@ -10,11 +11,9 @@ import java.util.Date;
 public class Teste {
 
     public static void main(String[] args) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        calendar.add(Calendar.DAY_OF_MONTH, 7);
-
-        Date dataProximaSemana = calendar.getTime();
-        System.out.println(dataProximaSemana.toString());
+        CategoriaModel c = new CategoriaModel(new CategoriaCadastro("teste", "url qualquer"));
+        
+        EventoDao e = new EventoDao();
+        e.buscarPelaCategoria(c);
     }
 }
