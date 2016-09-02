f44fc57e61ebb8035ffa9b4b089757df8b2d864c
Anderson <15153766@LAB10-09.suicobrasileira.sp>
2016-09-02 13:15:25 -0300

    teste filtro de eventos por categoria

--- src/main/java/com/platz/util/Teste.java
+++ src/main/java/com/platz/util/Teste.java
@@ -19,7 +19,7 @@ public class Teste {
         CategoriaModel cat = new CategoriaDao().buscarPorId(CategoriaModel.class, "57c8805c05b3981224216f45");
 
         List<EventoModel> lista = new EventoDao().buscarPelaCategoria(cat);
-        
+                
         for (EventoModel eventoModel : lista) {
             System.out.println(cat.getNome());
             System.out.println(eventoModel.getNome());
