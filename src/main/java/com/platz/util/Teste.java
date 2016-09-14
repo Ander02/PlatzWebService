cb62d56889e207d59aa6197b9401bf6c2a7716b7
Anderson <15153766@LAB10-09.suicobrasileira.sp>
2016-09-14 16:04:36 -0300

    Método para a contagem de linhas do projeto

--- src/main/java/com/platz/util/Teste.java
+++ src/main/java/com/platz/util/Teste.java
@@ -1,14 +1,53 @@
 package com.platz.util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author Anderson
  */
 public class Teste {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        //Verificando o número de linhas no projeto
+        final String folderPath = "C:\\Users\\15153766\\Documents\\NetBeansProjects\\PlatzWebService\\src";
 
-        System.out.println("252effbedc50d8600f714a9d1f1820f7e87061ef000de3540457c6eab8f7a460437f8e8e7b7a4fc90e45d53c971412ab53135743fd20e1e871cef0df3ea69ed7".length());
-
+        long totalLineCount = 0;
+        final List<File> folderList = new LinkedList<>();
+        folderList.add(new File(folderPath));
+        while (!folderList.isEmpty()) {
+            final File folder = folderList.remove(0);
+            if (folder.isDirectory() && folder.exists()) {
+                System.out.println("Scanning " + folder.getName());
+                final File[] fileList = folder.listFiles();
+                for (final File file : fileList) {
+                    if (file.isDirectory()) {
+                        folderList.add(file);
+                    } else if (file.getName().endsWith(".java")
+                            || file.getName().endsWith(".xml")) {
+                        long lineCount = 0;
+                        final Scanner scanner = new Scanner(file);
+                        while (scanner.hasNextLine()) {
+                            scanner.nextLine();
+                            lineCount++;
+                        }
+                        totalLineCount += lineCount;
+                        final String lineCountString;
+                        if (lineCount > 99999) {
+                            lineCountString = "" + lineCount;
+                        } else {
+                            final String temp = ("     " + lineCount);
+                            lineCountString = temp.substring(temp.length() - 5);
+                        }
+                        System.out.println(lineCountString + " lines in " + file.getName());
+                    }
+                }
+            }
+        }
+        System.out.println("Scan Complete: " + totalLineCount + " lines total");
     }
 }
