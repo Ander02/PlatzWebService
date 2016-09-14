97e46ea2e4d98cdb5f6a16fae53a13e79bfa3dee
Anderson <15153766@LAB10-09.suicobrasileira.sp>
2016-09-14 14:44:20 -0300

    Organizando comentários das daos

--- src/main/java/com/platz/dao/GenericDao.java
+++ src/main/java/com/platz/dao/GenericDao.java
@@ -4,11 +4,6 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import org.bson.types.ObjectId;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author Anderson
