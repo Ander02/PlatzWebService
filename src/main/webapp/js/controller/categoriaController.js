2d6cde67ded1865bf0dcf58bed694ac956069998
15153770 <15153770@LAB10-02.suicobrasileira.sp>
2016-09-29 16:41:06 -0300

    Contrução de erros em categoria controller

--- src/main/webapp/js/controller/categoriaController.js
+++ src/main/webapp/js/controller/categoriaController.js
@@ -1,63 +1,73 @@
 angular.module("platz").controller("categoriaController", function ($scope, $http, toastr) {
 
     //Faz um Get no Web Service recebendo uma URL
+
+    //funções que realizam consulta no webService
     $scope.listarTodos = function () {
         $http.get(webService + "/categorias").then(function (response) {
-            $scope.categoriasAll = response.data;            
+            $scope.categoriasAll = response.data;
         }, function (response) {
+            erro(errorManager(response.config.url, response.status, "Erro ao listar categorias"));
         });
     };
     $scope.listarExcluidas = function () {
         $http.get(webService + "/categorias/excluidas").then(function (response) {
             $scope.categoriasExcluidas = response.data;
-        }, function (response) {
+        }, function (response) {            
+            erro(errorManager(response.config.url, response.status, "Erro ao listar categorias excluidas"));
         });
     };
     $scope.listarNãoExcluidas = function () {
         $http.get(webService + "/categorias/naoExcluidas").then(function (response) {
             $scope.categorias = response.data;
         }, function (response) {
+            erro(errorManager(response.config.url, response.status, "Erro ao listar categorias"));
         });
     };
+
     $scope.alterar = function () {
         $http.put(webService + "/categoria/" + $scope.categoriaEdicao.id, $scope.categoriaEditada).then(function (response) {
             atualizar();
             $scope.categoriaEditada = null;
+            alterado("Categoria editar com sucesso");
             sleep(1000);
             location.reload();
         }, function (response) {
-
+            erro(errorManager(response.config.url, response.status, "Erro ao alterar categoria"));
         });
     }
-    $scope.cadastrar = function (categoria) {
+    $scope.cadastrar = function () {
 
-        console.log(categoria);
-
-        $http.post(webService + "/categoria", categoria).then(function (response) {
-
-            console.log("Categoria Cadastrada com Sucesso");
-
-            //Atualiza a lista
+        $http.post(webService + "/categoria", $scope.categoriaCadastro).then(function (response) {
             atualizar();
-            
-            toastr.success('Categoria cadastrada com sucesso', 'Sucesso!',{
-               progressBar: true
-            });
+            $scope.categoriaCadastro = null;
+            sucesso("Categoria cadastrada com sucesso");
         }, function (response) {
-
-            console.log("Erro ao cadastrar");
-            console.log(response);
-
+            erro(errorManager(response.config.url, response.status, "Erro ao cadastrar categoria"));
         });
     };
     $scope.recuperar = function () {
         $http.put(webService + "/categoria/recuperar/" + $scope.categoriaRecuperacaoId).then(function (response) {
             atualizar();
-            alert("categoria recuperada com sucesso");
+            sucesso("Categoria recuperada com sucesso");
         }, function (reponse) {
-
+            erro(errorManager(response.config.url, response.status, "Erro ao recuperar categoria"));
         });
     };
+
+    $scope.deletar = function () {
+
+        console.log($scope.categoriaExclusaoId);
+
+        $http.delete(webService + "/categoria/" + $scope.categoriaExclusaoId).then(function (response) {
+            atualizar();
+            excluido("Categoria deletada com sucesso");
+        }, function (response) {
+            erro(errorManager(response.config.url, response.status, "Erro deletar categoria"));
+        });
+    };
+
+    //funções de preparação de variaveis
 
     $scope.prepararRecuperacao = function (id) {
         $scope.categoriaRecuperacaoId = id;
@@ -82,21 +92,7 @@ angular.module("platz").controller("categoriaController", function ($scope, $htt
         $scope.categoriaExclusaoId = null;
     };
 
-    $scope.deletar = function () {
-
-        console.log($scope.categoriaExclusaoId);
-
-        $http.delete(webService + "/categoria/" + $scope.categoriaExclusaoId).then(function (response) {
-
-            atualizar();
-            alert("deletado");
-            console.log(response);
-
-        }, function (response) {
-
-        });
-    };
-
+    //funções de atualizações e avisos
     function atualizar() {
         $scope.listarTodos();
         $scope.listarExcluidas();
@@ -106,7 +102,54 @@ angular.module("platz").controller("categoriaController", function ($scope, $htt
         $scope.cancelarRecuperacao();
     }
     window.onload = atualizar();
+
+    function sucesso(mensagem) {
+        toastr.success(mensagem, 'Sucesso', {
+            progressBar: true,
+            closeButton: true,
+            timeOut: 5000,
+            extendTimeOut: 2000
+        });
+    }
+    function excluido(mensagem) {
+        toastr.success(mensagem, 'Deletado', {
+            progressBar: true,
+            closeButton: true,
+            timeOut: 5000,
+            extendTimeOut: 2000
+        });
+    }
+    function alterado(mensagem) {
+        toastr.success(mensagem, 'Alterado', {
+            progressBar: true,
+            closeButton: true,
+            timeOut: 5000,
+            extendTimeOut: 2000
+        });
+    }
+
+    function info(mensagem) {
+        toastr.info(mensagem, 'Informações', {
+            progressBar: true,
+            closeButton: true,
+            timeOut: 5000,
+            extendTimeOut: 2000
+        });
+    }
+    function erro(mensagem) {
+        toastr.error(mensagem, 'Erro', {
+            progressBar: true,
+            closeButton: true,
+            timeOut: 0,
+            extendTimeOut: 3000
+        });
+    }
+    function aviso(mensagem) {
+        toastr.warning(mensagem, 'Aviso', {
+            progressBar: true,
+            closeButton: true,
+            timeOut: 0,
+            extendTimeOut: 3000
+        });
+    }
 });
-
-
-
--- src/main/webapp/js/util.js
+++ src/main/webapp/js/util.js
@@ -14,28 +14,28 @@ function sleep(milliseconds) {
 function errorManager(erro, status, mensagem) {
     switch (status) {
         case 200:
-            alert("Status ok: erro não identificado" + " - " + erro);
+            return "Status: ok. - erro não identificado" + " - " + erro;
             break;
         case 201:
-            alert("Status Criado: erro não identificado" + " - " + erro);
+            return "Status: Criado. - erro não identificado" + " - " + erro;
             break;
         case 400:
-            alert("Status Bad Request: " + mensagem + " - " + erro);
+            return "Status: Bad Request. - " + mensagem + " - " + erro;
             break;
         case 401:
-            alert("Status Não autorizado: " + mensagem + " - " + erro);            
+            return "Status: Não autorizado. - " + mensagem + " - " + erro;
             break;
         case 403:
-            alert("Status Proibido: " + mensagem + " - " + erro);
+            return "Status: Proibido. - " + mensagem + " - " + erro;
             break;
         case 404:
-            alert("Status Não Encotrado: " + mensagem + " - " + erro);
+            return "Status: Não Encontrado. - " + mensagem + " - " + erro;
             break;
         case 500:
-            alert("Erro de servidor, por favor contate o administrador do sitema e relate o erro: " + mensagem + " - " + erro);
+            return "Erro de servidor, por favor contate o administrador do sitema e relate o erro: " + mensagem + " - " + erro;
             break;
         default:
-            alert("Erro não identificado. " + mensagem + " - " + erro);
+            return "Erro não identificado. " + mensagem + " - " + erro;
             break;
     }
 }
