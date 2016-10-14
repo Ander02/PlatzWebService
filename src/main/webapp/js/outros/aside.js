74168e55fe622edf110c3008c5dab6261275f2cf
15153781 <15153781@LAB10-19.suicobrasileira.sp>
2016-10-14 16:12:11 -0300

    ajuste na nav de fora

---
+++ src/main/webapp/js/outros/aside.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var link = document.getElementById("mySidenav")
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
